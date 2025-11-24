fe5bcd533683ec50c08f8635ac34bbbde94cd096
Hans Henrik Appel <hha@haheap.com>
2025-11-24 09:06:38 +0100

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Haheap\Zendo;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
